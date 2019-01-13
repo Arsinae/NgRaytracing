ee068625f4a9f6b951a5a149b35a1465f72beeaf
Thibaud Huber <thibaud.huber@epitech.eu>
2019-01-13 18:55:18 -0500

    Change default configuration

--- src/app/raytracer/dataClass.ts
+++ src/app/raytracer/dataClass.ts
@@ -1,16 +1,16 @@
 export class Position {
   public x = 0;
   public y = 0;
-  public z = 100;
+  public z = 600;
 
   constructor() {
   }
 }
 
 class Color {
-  public r = 255;
+  public r = 0;
   public g = 0;
-  public b = 0;
+  public b = 255;
   public a = 1;
 }
 
@@ -18,9 +18,9 @@ export class FormClass {
 
   public type = 'sphere'; // Can be sphere, cylinder, cone, hyperbole
   public pos = new Position(); // Position of the object; {0, 0, 0} is the center
-  public r = 50; // Radius of the object
-  public size = 100; // Size of the object
-  public center = 25; // Size of the center for the Hyperbole
+  public r = 500; // Radius of the object
+  public size = 500; // Size of the object
+  public center = 250; // Size of the center for the Hyperbole
   public color = new Color();
 
   constructor() {
